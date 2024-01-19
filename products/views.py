8d38e3e0fcb5e48a83a4db56da481ebc0b4beaf4
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-01-19 17:33:20 +0000

    Editing Products

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            product = form.save()
+            form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('add_product'))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -113,11 +113,3 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
-
-
-def delete_product(request, product_id):
-    """ Delete a product from the store """
-    product = get_object_or_404(Product, pk=product_id)
-    product.delete()
-    messages.success(request, 'Product deleted!')
-    return redirect(reverse('products'))
