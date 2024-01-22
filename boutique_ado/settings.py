ef690c9dab752bc1ddfb21caf804265dc194caa0
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2024-01-22 11:09:00 +0000

    Allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
-from pathlib import Path
+#from pathlib import Path
 if os.path.exists("env.py"):
   import env 
 
